ec99e6fdaf68cf3698054a6c034d2acfe1fa3dc4
Elliot Henriquez <elliothenriq@gmail.com>
2022-03-15 00:06:52 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+
+// include the Node.js 'path' module at the top of your file
+const path = require('path')
+
+// modify your existing createWindow() function
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 680,
+    minWidth: 940,
+    minHeight: 560,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
